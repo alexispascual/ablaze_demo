367fadc3fcce4427b7d7be8e97a0081f15f76495
alexispascual <ap.alexispascual@gmail.com>
2022-09-13 22:23:12 -0400

    Can now display a modal with text

--- js/script.js
+++ js/script.js
@@ -24,16 +24,31 @@ jQuery(document).ready(function($){
 
 function img_popup() {
   const img = document.querySelector("img")
-
+  const modal = document.getElementById("myModal");
+  const captionText = document.getElementById("caption");
+  const span = document.getElementsByClassName("close")[0];
+  
   img.onclick = (e) => {
     const x = e.pageX - e.target.offsetLeft;
     const y = e.pageY - e.target.offsetTop;
     //console.log(x, y);
     if((0.51 < y/img.clientHeight > 0.78) && (0.52 < x/img.clientWidth > 0.84)) {
-       console.log("Bench selected");
-       }
+      console.log("Bench selected");
+      modal.style.display = "block";
+      modal.style.left = e.pageX + "px";
+      modal.style.top = e.pageY + "px";
+      captionText.innerHTML = "This is a bench"; 
+      
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() { 
+        modal.style.display = "none";
+      }
+    }
+    
+    else {
+      modal.style.display = "none";
+    }
   }
-
 }
 
 img_popup();
