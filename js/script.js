5ad52f0bed187ad6d3b11d994ade4832c20e3d37
alexispascual <ap.alexispascual@gmail.com>
2022-09-19 22:14:03 -0400

    Adding image outline

--- js/script.js
+++ js/script.js
@@ -23,30 +23,48 @@ jQuery(document).ready(function($){
 });
 
 function img_popup() {
-  const img = document.querySelector("img")
+  const img = document.getElementById("img-bottom")
   const modal = document.getElementById("myModal");
   const captionText = document.getElementById("caption");
   const span = document.getElementsByClassName("close")[0];
   
+  var toggled = false;
+  
+  
   img.onclick = (e) => {
-    const x = e.pageX - e.target.offsetLeft;
-    const y = e.pageY - e.target.offsetTop;
-    //console.log(x, y);
-    if((0.51 < y/img.clientHeight > 0.78) && (0.52 < x/img.clientWidth > 0.84)) {
+    var offset = img.getBoundingClientRect();
+    const x = e.pageX - offset.left;
+    const y = e.pageY - offset.top;
+    
+    if((x > 307 && x < 579) && (y > 238 && y < 375)) {
       console.log("Bench selected");
+
       modal.style.display = "block";
       modal.style.left = e.pageX + "px";
       modal.style.top = e.pageY + "px";
       captionText.innerHTML = "This is a bench"; 
       
+      if (!toggled) {
+        $("#object-highlight img.top").toggleClass("transparent");
+        toggled = true;
+      }
+      
       // When the user clicks on <span> (x), close the modal
       span.onclick = function() { 
         modal.style.display = "none";
+        if (toggled) {
+          $("#object-highlight img.top").toggleClass("transparent");
+          toggled = false;
+        }
       }
     }
     
     else {
       modal.style.display = "none";
+      if (toggled) {
+        $("#object-highlight img.top").toggleClass("transparent");
+        toggled = false;
+      }
     }
   }
 }
